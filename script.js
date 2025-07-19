00e6e66915d81f4c5b5174fca281bf2c6a569743
Luiz Fernando <luiz.fernandosv23@gmail.com>
2025-07-19 15:40:32 -0300

    Função de remoção

--- script.js
+++ script.js
@@ -33,6 +33,10 @@ form.addEventListener('submit',(event)=> {
     ExibirPessoa(novaPessoa)
     // Atualiza o array
     localStorage.setItem('pessoas',JSON.stringify(pessoasStorage))
+
+    // Limpa os campos
+    form.querySelector('#campo_nome').value = ''
+    form.querySelector('#campo_nascimento').value = ''
 })
 
 // Função para exibir as pessoas na tabela
@@ -50,24 +54,34 @@ function ExibirPessoa(pessoa,indice){
     let tdNascimento = document.createElement('td');
     // Atribui o valor da célula com a data de nascimento do objeto
     tdNascimento.textContent = pessoa.nascimento;
-    // Cria uma célula para guardar o botão de editar
+    // Cria uma célula para guardar o botão de editar e remover
     let tdEditar = document.createElement('td');
-    // Cria o botão
+    let tdRemover = document.createElement('td');
+    // Cria os botões
     let btnEditar = document.createElement('button');
-    // Adiciona o texto
+    let btnRemover = document.createElement('button')
+    // Adiciona os textos
     btnEditar.textContent = "Editar";
-    // Adiciona uma classe
+    btnRemover.textContent = "Remover"
+    // Adiciona as classe
     btnEditar.classList.add('btn_editar');
+    btnRemover.classList.add('btn_remover')
     // Adiciona uma função para editar os dados
     btnEditar.addEventListener('click', function(){
         editarPessoa(indice,pessoa.nome,pessoa.nascimento)
     })
+    // Adiciona uma função para remover os dados
+    btnRemover.addEventListener('click',function(){
+        removerPessoa(indice)
+    })
     // Adiciona o botão a célula
     tdEditar.appendChild(btnEditar)
+    tdRemover.appendChild(btnRemover)
     // Adiciona as 3 células na nova linha
     novaLinha.appendChild(tdNome);
     novaLinha.appendChild(tdNascimento);
     novaLinha.appendChild(tdEditar);
+    novaLinha.appendChild(tdRemover)
     // Adiciona a nova linha no tbody
     conteudo_tabela.appendChild(novaLinha);
 }
@@ -89,6 +103,19 @@ function editarPessoa(indice, nome, nascimento) {
     campo_nascimento.value = nascimento;
 }
 
+function removerPessoa(indice){
+    // Atualiza a tabela
+    let conteudo_tabela = document.querySelector('.conteudo-tabela');
+    let linha = conteudo_tabela.querySelector(`tr[data-index="${indice}"]`);
+
+    // Remove a linha
+    linha.remove();
+    // Remove a pessoa do array
+    pessoasStorage.splice(indice,1)
+    // Atualiza o array
+    localStorage.setItem('pessoas',JSON.stringify(pessoasStorage));
+}
+
 // Adiciona o event listener apenas uma vez
 form_editar.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -100,8 +127,8 @@ form_editar.addEventListener('submit', (event) => {
     let campo_nascimento = form_editar.querySelector('#campo_nascimento_editar');
 
     // Captura os novos valores
-    const novoNome = campo_nome.value;
-    const novoNascimento = campo_nascimento.value;
+    let novoNome = campo_nome.value;
+    let novoNascimento = campo_nascimento.value;
 
     // Atualiza o array no índice necessário
     pessoasStorage[indiceEmEdicao].nome = novoNome;
